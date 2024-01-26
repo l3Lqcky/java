a1408e3ec55caa75e3b37143c421d544afa0bf10
l3Lqcky <ktq2390311@stu.o-hara.ac.jp>
2024-01-26 14:09:09 +0900

    Commit

---
+++ ensyuu14/mozisousa.java
@@ -0,0 +1,14 @@
+package ensyuu14;
+
+import java.util.Scanner;
+
+public class mozisousa {
+
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("文字列を入力: ");
+        String inputString = stdIn.nextLine(); 
+        System.out.println(inputString); 
+    }
+}
